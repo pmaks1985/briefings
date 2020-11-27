1ac63f7e1d0fe5ac4693ded2596276b2f1386a46
pmaks1985 <pmaks1985@gmail.com>
2020-11-27 17:04:10 +0300

    перенес на свой сервер, сделал плитки, при нажатии открывается видео. Не забыть сделать изменение class, сейчас сделал в ручную

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 jQuery(function () {
-    jQuery("video__youtube").YouTubePopUp();
+    jQuery(".video__youtube_1").YouTubePopUp();
+    jQuery(".video__youtube_2").YouTubePopUp();
+    jQuery(".video__youtube_3").YouTubePopUp();
 });
